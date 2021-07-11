2a2902f7d6d0d2e7a2c4a026632faac45e968262
muka8 <kzkazuki@icloud.com>
2021-07-11 12:38:38 +0900

    ノブロッッングななSubscribe

--- src/subscriber_async.cpp
+++ src/subscriber_async.cpp
@@ -2,11 +2,14 @@
 #include <tutorial_topic/Int32Stamped.h>
 #include <mutex>
 //グローバル変数の定義
-static tutorial_topic::Int32Stamped g_number
+static tutorial_topic::Int32Stamped g_number;
 static std::mutex g_mutex;
 
+//Topicを受信すると呼ばれるコールバック関数
+void onNumberSubscribed(const tutorial_topic::Int32Stamped &msg)
+{
 //関数を抜けるまでmutexをlock
- std::look_guard<std::mutex> lock(g_mutex);
+ std::lock_guard<std::mutex> lock(g_mutex);
 //受信したTopicをコピー
  g_number = msg;
 }
@@ -17,13 +20,23 @@ int main(int argc, char **argv)
  ros::init(argc, argv, "Subscriber");
  ros::NodeHandle nh;
 //SubscribeするTopicの設定をコールバック関数の登録 
- ros::Subscriber sub = nh.subscribe("number", 10, onNumberSubscribed)
+ ros::Subscriber sub = nh.subscribe("number", 10, onNumberSubscribed);
 //ループ間隔の設定
  ros::Rate loop_rate(1);
 //AsyncSpinnerの初期化
  ros::AsyncSpinner spinner(1);
  spinner.start();
 //loop
-while(ros::ok){
+ while(ros::ok){
 //mutexをlock
+ g_mutex.lock();
+ //受信したTopicデータの表示
+ ROS_INFO("Number : %d, Time : %1f", g_number.data, g_number.header.stamp.toSec());
+ //mutexをunlock
+ g_mutex.unlock();
+ //周期が1秒になるように待機
+ loop_rate.sleep();
+ }
+ return 0;
+}
 
